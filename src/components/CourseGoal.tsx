fa95482533083ca6babc31916667c0748f9a9eda
eclimov <klimovedik88@gmail.com>
2025-04-12 14:33:40 +0300

    - Added props to CourseGoal component

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,18 @@
-export default function CourseGoal() {
+import { type PropsWithChildren } from 'react';
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        {children}
       </div>
       <button>Delete</button>
     </article>
